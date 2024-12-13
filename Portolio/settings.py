de9cca2c966edf502a74d09303817e49a76ca115
Andreas S. Wegund <andreas.wegund@gmail.com>
2024-12-13 14:25:39 +0100

    changes for Cloudinary storage

--- Portolio/settings.py
+++ Portolio/settings.py
@@ -195,14 +195,14 @@ STATICFILES_DIRS = [ BASE_DIR / 'static' ]
 MEDIA_URL = '/media/'
 if DEBUG == False:
       DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+      CLOUDINARY_STORAGE = {
+            'CLOUD_NAME': os.environ.get( "CLOUDINARY_CLOUD_NAME" ),
+            'API_KEY':    os.environ.get( "CLOUDINARY_API_KEY" ),
+            'API_SECRET': os.environ.get( "CLOUDINARY_API_SECRET" ),
+      }
+
 else:
       MEDIA_ROOT = BASE_DIR / 'media'
-
-CLOUDINARY_STORAGE = {
-      'CLOUD_NAME': os.environ.get( "CLOUDINARY_CLOUD_NAME" ),
-      'API_KEY':    os.environ.get( "CLOUDINARY_API_KEY" ),
-      'API_SECRET': os.environ.get( "CLOUDINARY_API_SECRET" ),
-}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
