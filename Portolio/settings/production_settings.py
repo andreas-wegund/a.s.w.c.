b045199a2322f364c089236dd7b08137d27507c8
Andreas S. Wegund <andreas.wegund@gmail.com>
2024-12-19 14:45:37 +0100

    error in setting.py

--- Portolio/settings/production_settings.py
+++ Portolio/settings/production_settings.py
@@ -48,7 +48,6 @@ CLOUDINARY_STORAGE = {
       'API_KEY':    os.environ.get( "CLOUDINARY_API_KEY" ),
       'API_SECRET': os.environ.get( "CLOUDINARY_API_SECRET" ),
 }
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MEDIA_URL = '/media/'  # or any prefix you choose
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
